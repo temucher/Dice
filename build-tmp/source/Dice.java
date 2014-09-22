bd6f217602b7b7dedcddc90187ed8d3d23b23c7c
temucher <tjsmuch@gmail.com>
2014-09-22 12:13:41 -0700

    My Dice program

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,16 +1,32 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 //Teagan Mucher, "Dice", Block 4 AP Computer Science
 
 Die omega;
 Die theta;
 int total, numDots;
-void setup()
+public void setup()
 {
 	size(500,500);
 	noLoop();
 	omega = new Die(50, 50);
 	theta = new Die(200, 50);
 }
-void draw()
+public void draw()
 {	
 	total = 0;
 	omega.roll();
@@ -22,7 +38,7 @@ void draw()
 	textSize(20);
 	text(total, 250, 250);
 }
-void mousePressed()
+public void mousePressed()
 {
 	background(195);
 	redraw();
@@ -35,11 +51,11 @@ class Die //models one single dice cube
 		myX = x;
 		myY = y;
 	}
-	void roll()
+	public void roll()
 	{
 		numDots = (int)(Math.random()*6+1);
 	}
-	void show()
+	public void show()
 	{
 		fill(255);
 		rect(myX, myY, 100, 100);
@@ -116,4 +132,16 @@ class Die //models one single dice cube
 			}
 		}	
 	}
+	// total = total + numDots;
+	// textSize(20);
+	// text(total, 250, 250);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
